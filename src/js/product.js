4eefadd874cfe48a3a832aa9e0a33287031acc23
Luiz Felipe OP <luiz.felipe221295@hotmail.com>
2022-01-25 11:30:35 -0300

    feat: Layout Products DOM

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,13 @@
 class Product{
     constructor(name,photo,description,category,price,id,updatedAt,createdAt){
         this.name = name
+        this.photo = photo
         this.description = description
         this.category = category
         this.price = price
         this.id = id
         this.updatedAt = updatedAt
-        this.createdAt = createdAt
-        this.photo = photo
-    }
+        this.createdAt = createdAt  
+    } 
 }
+
