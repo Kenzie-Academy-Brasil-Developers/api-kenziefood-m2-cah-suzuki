3bc4c25bc5ffc8e9aa814057c03ffa36ec8b40ae
camila suzuki <camila.suzuki.p@gmail.com>
2022-01-27 11:23:00 -0300

    apiclass refatorada

--- src/js/apiClass.js
+++ src/js/apiClass.js
@@ -12,55 +12,59 @@ class ApiClass{
         // }
     }
 
-    fetchProdutosId (id) {
-      return fetch("https://kenzie-food-api.herokuapp.com/product/" + id)
-      .then(response => {
-        var listaDeProdutos = response.json()
-        return listaDeProdutos
-      })
-    
+    async fetchProdutosId (id) {
+      let response = await fetch("https://kenzie-food-api.herokuapp.com/product/" + id)
+         return await response.json()
+
   }
 
-     postProdutos (data) {
-      const response =  fetch("https://kenzie-food-api.herokuapp.com/my/product",{
-        method: "post",
-        headers:{
-          "Content-Type":"application/json",
-          Authorization:"Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImlhdCI6MTY0MzEzMjU2MywiZXhwIjoxNjQzOTk2NTYzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.Efp7V278ellXsdZwL5o2tx1aIkIowpXymkWktRUtE-8",
-        },
-        body:JSON.stringify(data),
-      }).then(response => {
-        let produtoCriado = response.json()
-        console.log(produtoCriado)
-        return produtoCriado
-      })
+    //  postProdutos (data) {
+    //   const response =  fetch("https://kenzie-food-api.herokuapp.com/my/product",{
+    //     method: "post",
+    //     headers:{
+    //       "Content-Type":"application/json",
+    //       Authorization:"Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImlhdCI6MTY0MzEzMjU2MywiZXhwIjoxNjQzOTk2NTYzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.Efp7V278ellXsdZwL5o2tx1aIkIowpXymkWktRUtE-8",
+    //     },
+    //     body:JSON.stringify(data),
+    //   }).then(response => {
+    //     let produtoCriado = response.json()
+    //     console.log(produtoCriado)
+    //     return produtoCriado
+    //   })
+    // }
+
+    async postProdutos(data){
+      let response = fetch("https://kenzie-food-api.herokuapp.com/my/product",{
+            method: "post",
+            headers:{
+              "Content-Type":"application/json",
+              Authorization:"Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImlhdCI6MTY0MzEzMjU2MywiZXhwIjoxNjQzOTk2NTYzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.Efp7V278ellXsdZwL5o2tx1aIkIowpXymkWktRUtE-8",
+            },
+            body:JSON.stringify(data),
+          })
+          return await response.json()
     }
 
-    getMeusProdutos () {
-      return fetch("https://kenzie-food-api.herokuapp.com/my/product",{
-        headers:{
-          "Content-Type":"application/json",
-          Authorization:"Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImlhdCI6MTY0MzEzMjU2MywiZXhwIjoxNjQzOTk2NTYzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.Efp7V278ellXsdZwL5o2tx1aIkIowpXymkWktRUtE-8",
+    async getMeusProdutos () {
+      let response = await fetch("https://kenzie-food-api.herokuapp.com/my/product", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImlhdCI6MTY0MzEzMjU2MywiZXhwIjoxNjQzOTk2NTYzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.Efp7V278ellXsdZwL5o2tx1aIkIowpXymkWktRUtE-8",
         },
       })
-      .then(response => {
-        var listaDeProdutos = response.json()
-        return listaDeProdutos
-      })
+      return await response.json()
   }
 
-    patchProdutos (data,id) {
-      const response =  fetch("https://kenzie-food-api.herokuapp.com/my/product/" + id,{
+    async patchProdutos (data,id) {
+      let response =  await fetch("https://kenzie-food-api.herokuapp.com/my/product/" + id,{
         method: "patch",
         headers:{
           "Content-Type":"application/json",
           Authorization:"Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsImlhdCI6MTY0MzEzMjU2MywiZXhwIjoxNjQzOTk2NTYzLCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.Efp7V278ellXsdZwL5o2tx1aIkIowpXymkWktRUtE-8",
         },
         body:JSON.stringify(data),
-      }).then(response => {
-        let produtoCriado = response.json()
-        return produtoCriado
-      })
+      }) 
+      return await response.json()
     }
 
     async deleteProdutos(id){
